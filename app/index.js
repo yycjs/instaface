ee00dff966540dd0773f94b8eb21d0f7d1ec1fc5
Eric Kryski <e.kryski@gmail.com>
2014-06-17 18:26:34 -0600

    fixing some of the styles

--- app/index.js
+++ app/index.js
@@ -4,3 +4,6 @@ var path = require('path');
  
 feathers().use(feathers.static(path.join(__dirname, '..', 'public')))
   .listen(1337);
+
+console.log('Feathers Server running on 127.0.0.1:1337');
+    
